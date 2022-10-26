dce5f2637779b7fa679f9bad408679ac85eb762b
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-10-26 09:54:04 +0200

    Finalizado

--- app/src/main/java/com/example/piedrapapeltijeraslagartospok/MainActivity.kt
+++ app/src/main/java/com/example/piedrapapeltijeraslagartospok/MainActivity.kt
@@ -69,6 +69,15 @@ class MainActivity : AppCompatActivity(), Comunicador {
     }
 
     private fun marcador() {
+        var txtMarcadorCpu: TextView = findViewById(R.id.txtMarcCPU)
+        var txtMarcadorUser: TextView = findViewById(R.id.txtMarcUser)
+        var random = Random.nextInt(Armas.values().size)
+        var arma: Armas = Armas.values().get(random)
+        var imgCpu: ImageView = findViewById(R.id.imgCPU)
+        var imgUser : ImageView = findViewById(R.id.imgUser)
+        var winner = findViewById<ImageView>(R.id.imgWinner)
+        var winner2 = findViewById<ImageView>(R.id.imgWinner2)
+        var txtWinner = findViewById<TextView>(R.id.txtWinner)
         when (imgUserRes) {
             Armas.PIEDRA.resourceID -> {
                 when (imgCpuRes) {
@@ -151,6 +160,26 @@ class MainActivity : AppCompatActivity(), Comunicador {
                 }
             }
         }
+        //Establezco cuando ganará uno de los dos
+        if(marcadorCpu == maxPuntuacion || marcadorUser == maxPuntuacion){
+            if(marcadorCpu > marcadorUser) {
+                txtWinner.text = "CPU"
+                winner.visibility = VISIBLE
+            }else{
+                txtWinner.text = "USUARIO"
+
+                winner2.visibility = VISIBLE
+            }
+
+            //Quito de la pantalla tódo lo que no se debe ver en ese momento
+            //Y pongo la imagen de la copa ademas del nombre del ganador
+            findViewById<Button>(R.id.btnLagarto).isVisible= false
+            findViewById<Button>(R.id.btnSpok).isVisible = false
+            findViewById<Button>(R.id.btnTijeras).isVisible = false
+            findViewById<Button>(R.id.btnPapel).isVisible = false
+            findViewById<Button>(R.id.btnPiedra).isVisible = false
+            findViewById<Button>(R.id.txtVs).isVisible = false
+        }
     }
 
     fun cambioImagenCpu() {
@@ -164,31 +193,6 @@ class MainActivity : AppCompatActivity(), Comunicador {
         var winner = findViewById<ImageView>(R.id.imgWinner)
         var txtWinner = findViewById<TextView>(R.id.txtWinner)
 
-        //Establezco cuando ganará uno de los dos
-        if(marcadorCpu == maxPuntuacion || marcadorUser == maxPuntuacion){
-
-
-            if(marcadorCpu > marcadorUser) {
-                txtWinner.text = "CPU"
-            }else{
-                txtWinner.text = "USUARIO"
-            }
-
-            //Quito de la pantalla tódo lo que no se debe ver en ese momento
-            //Y pongo la imagen de la copa ademas del nombre del ganador
-            winner.visibility = VISIBLE
-            txtMarcadorCpu.visibility = GONE
-            txtMarcadorUser.visibility = GONE
-            imgCpu.visibility = GONE
-            imgUser.visibility = GONE
-            txtWinner.visibility = VISIBLE
-            findViewById<Button>(R.id.btnLagarto).isVisible= false
-            findViewById<Button>(R.id.btnSpok).isVisible = false
-            findViewById<Button>(R.id.btnTijeras).isVisible = false
-            findViewById<Button>(R.id.btnPapel).isVisible = false
-            findViewById<Button>(R.id.btnPiedra).isVisible = false
-            findViewById<Button>(R.id.txtVs).isVisible = false
-        }else {
             //Cambiamos la imagen del cpu que tenemos guardada
             imgCpuRes = arma.resourceID
             //Establecemos la imamgen en el ImageView del CPU
@@ -199,7 +203,7 @@ class MainActivity : AppCompatActivity(), Comunicador {
             txtMarcadorCpu.text = marcadorCpu.toString()
             txtMarcadorUser.text = marcadorUser.toString()
 
-        }
+
 
     }
 
